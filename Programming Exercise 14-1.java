f1fd7eda7f2b411ce7c253e91b9a5f78fb8dbd17
Heloisa Santos <105313772+lollyca@users.noreply.github.com>
2022-06-17 13:41:54 -0700

    Add files via upload

---
+++ Programming Exercise 14-1.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class JBookQuote extends JFrame implements ActionListener{
+   FlowLayout flow = new FlowLayout();
+   JLabel msg1 = new  JLabel("To begin with,");
+   JLabel msg2 = new JLabel("Marley was dead.");
+   JButton button = new JButton("Click for source");
+   JLabel msg3 = new JLabel();
+   String title = "**    A Christmas Carol    **";
+    public JBookQuote() {
+        // Write your code here
+        add(msg1);
+        add(msg2);
+        add(msg3);
+        add(button);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+      // your code here
+      String bookTitle = title;
+      msg3.setText(bookTitle);
+    }
+
+    public static void main(String[] args) {
+        JBookQuote aFrame = new JBookQuote();
+        aFrame.setSize(300, 150);
+        aFrame.setVisible(true);
+    }
+}
