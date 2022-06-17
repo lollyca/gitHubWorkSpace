dc26fea4d0d2c8b287a16364c26507a74725d557
Heloisa Santos <105313772+lollyca@users.noreply.github.com>
2022-06-17 13:53:44 -0700

    Add files via upload

---
+++ Debugging Exercise 14-1.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class DebugFourteen1 extends JFrame {
+    public DebugFourteen1(int size) {
+        super("This is my frame");
+        setSize(size, size * 2);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        DebugFourteen1 frame = new DebugFourteen1(100);
+        frame.setVisible(true);
+    }
+}
